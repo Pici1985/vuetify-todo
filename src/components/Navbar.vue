f689e7b9ad9c460ada876010fa53fe5826deb36f
Balazs <balazs@frog.net>
2022-07-04 13:02:44 +0100

    List items
    added links to navigation drawer

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -14,7 +14,24 @@
         </v-app-bar>
 
         <v-navigation-drawer v-model="drawer" app class="primary">
-            <p class="error">test</p>
+            <v-list>
+                <v-list-item 
+                    v-for="link in links" 
+                    :key="link.text" 
+                    router :to="link.route"
+                >
+                    <v-list-item-action>
+                        <v-icon class="white--text">
+                            {{ link.icon }}
+                        </v-icon>
+                    </v-list-item-action>
+                    <v-list-item-content>
+                        <v-list-item-title class="white--text">
+                            {{ link.text }}
+                        </v-list-item-title>
+                    </v-list-item-content>
+                </v-list-item>
+            </v-list>
         </v-navigation-drawer>
     </nav>
 </template>
@@ -23,7 +40,12 @@
 export default {
     data(){
         return{
-            drawer: false
+            drawer: false,
+            links: [
+                { icon: 'dashboard', text: 'Dashboard', route: '/'},   
+                { icon: 'folder', text: 'My Projects', route: '/projects'},   
+                { icon: 'person', text: 'Team', route: '/team'}   
+            ]
         }
     }
 
