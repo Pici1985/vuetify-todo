22aeb9f76a6e6a258ae54b13e2404b37f9348643
Balazs <balazs@frog.net>
2022-07-05 15:11:04 +0100

    Dropdown menu
    created a dropdown on the main navbar

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -7,6 +7,45 @@
                 <span>App</span>
             </v-toolbar-title>
             <v-spacer></v-spacer>
+
+            <!-- dropwown menu  -->
+            <div class="text-center">
+                <v-menu offset-y>
+                    <template v-slot:activator="{ on, attrs }">
+                        <v-btn
+                            plain
+                            color="grey"
+                            text
+                            v-bind="attrs"
+                            v-on="on"
+                        >
+                            <v-icon left>
+                                expand_more
+                            </v-icon>
+                            <span>
+                                Menu
+                            </span>
+                        </v-btn>
+                    </template>
+                    <v-list>
+                        <v-list-item
+                            v-for="link in links"
+                            :key="link.text"
+                            router 
+                            :to="`${ link.route }`"
+                            color="primary"
+                        >
+                            <v-icon left>
+                                {{ link.icon }}
+                            </v-icon>
+                            <v-list-item-title>
+                                {{ link.text }}
+                            </v-list-item-title>
+                        </v-list-item>
+                    </v-list>
+                </v-menu>
+            </div>
+
             <v-btn plain color="grey">
                 <span>Sign Out</span>
                 <v-icon right> exit_to_app </v-icon>
