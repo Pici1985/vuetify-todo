cd24179164cb2115dc86ce87b32823105d84268f
Balazs <balazs@frog.net>
2022-07-05 14:13:02 +0100

    Cards
    created cards in team view

--- src/views/TeamView.vue
+++ src/views/TeamView.vue
@@ -3,14 +3,65 @@
         <h1 class="subheading grey--text">Team</h1>
 
         <v-container class="my-5">
-            content
+            <v-layout row wrap>
+                <v-flex xs12 sm6 md4 lg3 v-for="person in team" :key="person.name">
+                    <v-card class="text-center ma-3">
+                        <v-responsive class="pt-4">
+                            image goes here
+                        </v-responsive>
+                        <v-card-text>
+                            <div class="subheading">
+                                {{ person.name }}
+                            </div>
+                            <div class="grey--text">
+                                {{ person.role }}
+                            </div>                            
+                        </v-card-text>
+                        <v-card-actions>
+                            <v-btn text color="grey">
+                                <v-icon small float-left>
+                                    message
+                                </v-icon>
+                                <span>
+                                    Message
+                                </span>
+                            </v-btn>    
+                        </v-card-actions>
+                    </v-card>
+                </v-flex>
+            </v-layout>        
         </v-container>
     </div>
 </template>
 
 <script>
 export default {
-
+    data(){
+        return{
+            team: [
+                { 
+                    name: 'Chun Li', 
+                    role: 'Web Developer'
+                },
+                { 
+                    name: 'Ryu', 
+                    role: 'Graphic Designer'
+                },
+                { 
+                    name: 'The Net Ninja', 
+                    role: 'Web Developer'
+                },
+                { 
+                    name: 'Gouken', 
+                    role: 'Social Media Maverick'
+                },
+                { 
+                    name: 'Yoshi', 
+                    role: 'Sales Guru'
+                }
+            ]
+        }
+    }
 }
 </script>
 
