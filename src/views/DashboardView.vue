5fec026031526e77acac5fe36540e672b17d78ae
Balazs <balazs@frog.net>
2022-07-05 11:26:31 +0100

    Card created
    created a card with title, person, due by, and status

--- src/views/DashboardView.vue
+++ src/views/DashboardView.vue
@@ -3,7 +3,28 @@
       <h1 class="subheading grey--text">Dashboard</h1>
     
       <v-container class="my-5">
-        content
+
+        <v-card class="pa-5">
+          <v-layout row wrap>
+            <v-flex xs12 md6>
+              <div class="caption grey--text"> Project Title </div>
+              <div> Create new website </div>
+            </v-flex>
+            <v-flex xs6 sm4 md2>
+              <div class="caption grey--text"> Person </div>
+              <div>John Doe</div>
+            </v-flex>
+            <v-flex xs6 sm4 md2>
+              <div class="caption grey--text"> Due by: </div>
+              <div>1st Jan 2023</div>
+            </v-flex>
+            <v-flex xs2 sm4 md2>
+              <div class="caption grey--text">Status</div>
+              <div>ongoing</div>
+            </v-flex>
+          </v-layout>
+        </v-card>
+
       </v-container>
     </div>
 </template>
